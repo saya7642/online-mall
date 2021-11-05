ce0387b1aac0b57fc8ca81a7ca079b0df8ec35e0
sakura <691863195@qq.com>
2021-11-05 15:34:20 +0800

    first add

---
+++ mall-manage/src/main/java/com/example/Application.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        log.info("");
+    }
+}
